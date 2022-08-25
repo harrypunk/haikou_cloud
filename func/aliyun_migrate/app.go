2120ade2386b431fd96b9422b91fbbdd36907849
harry <allthatjazz@outlook.com>
2022-08-25 21:35:07 +0800

    Add db migration

--- func/aliyun_migrate/app.go
+++ func/aliyun_migrate/app.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/aliyun/fc-runtime-go-sdk/fc"
+	m "github.com/harrypunk/haikou_cloud/model"
+	"github.com/harrypunk/haikou_cloud/service"
 )
 
 type StructEvent struct {
-	Key string `json:"key"`
 }
 
-func HandleRequest(ctx context.Context, event StructEvent) (string, error) {
-	return fmt.Sprintf("hello, %s!", event.Key), nil
+func HandleRequest(ctx context.Context, event StructEvent) (*string, error) {
+	db, err := service.EnvConnection()
+	if err != nil {
+		return nil, err
+	}
+	err = db.AutoMigrate(
+		&m.Student{},
+		&m.Parent{},
+		&m.Family{},
+		&m.School{},
+		&m.Grade{},
+		&m.Course{},
+		&m.Teacher{},
+	)
+	if err != nil {
+		return nil, err
+	}
+	var msg = "db migration success"
+	return &msg, nil
 }
 
 func main() {
