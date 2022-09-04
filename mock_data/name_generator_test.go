d12ed3cefaad8b80769325f39fc5a346ffe8e113
harry <allthatjazz@outlook.com>
2022-09-04 19:37:48 +0800

    Set number of families by familyNum

--- mock_data/name_generator_test.go
+++ mock_data/name_generator_test.go
@@ -4,9 +4,26 @@ import (
 	"testing"
 )
 
-func TestNameGenerate(t *testing.T) {
-	gen := NewWithLen(10)
+func namesN(n int, t *testing.T) {
+	gen := NewWithLen(n)
+	count := 0
 	for names := range gen.GetNames() {
 		t.Logf("names: %v", names)
+		count += 1
 	}
+	if count != n {
+		t.Fail()
+	}
+}
+
+func TestNames5(t *testing.T) {
+	namesN(5, t)
+}
+
+func TestNames20(t *testing.T) {
+	namesN(20, t)
+}
+
+func TestNames40(t *testing.T) {
+	namesN(40, t)
 }
