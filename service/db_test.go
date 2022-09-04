d98a63c15eac29d6ac548b3892f17365fd71d5ae
harry <allthatjazz@outlook.com>
2022-09-04 22:04:25 +0800

    Add Grades Upsert

--- service/db_test.go
+++ service/db_test.go
@@ -3,6 +3,8 @@ package service
 import (
 	"os"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 func setLocalDB() {
@@ -12,10 +14,26 @@ func setLocalDB() {
 	os.Setenv("DB_URL", "localhost")
 }
 
+func localDB() (db *gorm.DB, err error) {
+	return Connection("onetwo", "", "localhost", "haikou_test")
+}
+
 func TestMigrate(t *testing.T) {
 	setLocalDB()
 	err := Migrate()
 	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestGrades(t *testing.T) {
+	db, err := localDB()
+	if err != nil {
+		t.Error(err)
+	}
+	err = AddGrades(db)
+	if err != nil {
+		t.Error(err)
 		t.Fail()
 	}
 }
--- service/mock_data.go
+++ service/mock_data.go
@@ -1,9 +1,47 @@
 package service
 
 import (
+	"github.com/harrypunk/haikou_cloud/model"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func AddMockData(db *gorm.DB) error {
 	return nil
+}
+
+func AddGrades(db *gorm.DB) error {
+	var m1 = model.Grade{
+		Model:       gorm.Model{ID: 101},
+		Num:         7,
+		DisplayText: "初一",
+	}
+	var m2 = model.Grade{
+		Model:       gorm.Model{ID: 102},
+		Num:         8,
+		DisplayText: "初二",
+	}
+	var m3 = model.Grade{
+		Model:       gorm.Model{ID: 103},
+		Num:         9,
+		DisplayText: "初三",
+	}
+	var h1 = model.Grade{
+		Model:       gorm.Model{ID: 104},
+		Num:         10,
+		DisplayText: "高一",
+	}
+	var h2 = model.Grade{
+		Model:       gorm.Model{ID: 105},
+		Num:         11,
+		DisplayText: "高二",
+	}
+	var h3 = model.Grade{
+		Model:       gorm.Model{ID: 106},
+		Num:         12,
+		DisplayText: "高三",
+	}
+	var grades = []model.Grade{m1, m2, m3, h1, h2, h3}
+	var result = db.Clauses(clause.OnConflict{UpdateAll: true}).Create(&grades)
+	return result.Error
 }
