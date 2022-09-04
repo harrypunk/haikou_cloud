5ac658def47e5ea2cc1e536033583d1102f8a53c
harry <allthatjazz@outlook.com>
2022-09-04 12:16:05 +0800

    Init mock data

--- service/db_test.go
+++ service/db_test.go
@@ -5,11 +5,15 @@ import (
 	"testing"
 )
 
-func TestMigrate(t *testing.T) {
+func setLocalDB() {
 	os.Setenv("DB_NAME", "haikou_test")
 	os.Setenv("DB_USER", "onetwo")
 	os.Setenv("DB_PASSWORD", "")
 	os.Setenv("DB_URL", "localhost")
+}
+
+func TestMigrate(t *testing.T) {
+	setLocalDB()
 	err := Migrate()
 	if err != nil {
 		t.Fail()
