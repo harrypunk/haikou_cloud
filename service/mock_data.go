a56e8ef9e359c939433b106359f9a44fa8d4f052
harry <allthatjazz@outlook.com>
2022-09-14 22:37:34 +0800

    Update package mock_data

--- service/mock_data.go
+++ service/mock_data.go
@@ -1,9 +1,9 @@
 package service
 
 import (
+	"github.com/harrypunk/haikou_cloud/mock_data"
 	"github.com/harrypunk/haikou_cloud/model"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 func AddMockData(db *gorm.DB) error {
@@ -13,28 +13,35 @@ func AddMockData(db *gorm.DB) error {
 }
 
 func AddMockSchool(db *gorm.DB) error {
-	var schools = []model.School{
-		school(33, "海南中学"),
-		school(34, "海口第一中学"),
-		school(35, "海口第二中学"),
-		school(36, "秀英第三中学"),
-		school(37, "慧蔚外国语"),
-	}
-	var result = db.Clauses(clause.OnConflict{DoNothing: true}).Create(&schools)
+	var result = db.Create(&mockSchools)
 	return result.Error
 }
 
-func school(id uint, name string) model.School {
+var mockSchools = []model.School{
+	school("海南中学"),
+	school("海口第一中学"),
+	school("海口第二中学"),
+	school("秀英第三中学"),
+	school("慧蔚外国语"),
+}
+
+func school(name string) model.School {
 	return model.School{
-		Model: gorm.Model{ID: id},
-		Name:  name,
+		Name: name,
 	}
 }
 
-// func parent(name string, gender uint8, phone string) model.Parent {
-// 	return model.Parent{
-// 		Name:   name,
-// 		Gender: gender,
-// 		Phone:  phone,
-// 	}
-// }
+func parent(name string, gender uint8, phone string) model.Parent {
+	return model.Parent{
+		Name:   name,
+		Gender: gender,
+		Phone:  phone,
+	}
+}
+
+func mockFamilies(seed int64, capacity int, bufferSize int) <-chan []model.Family {
+	var generator = mock_data.NewWithSeed(12)
+	var ch = make(chan []model.Family, capacity)
+
+	return ch
+}
