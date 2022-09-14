74c0319244608bb314d52515a4631a7a563e8bdf
harry <allthatjazz@outlook.com>
2022-09-14 23:28:15 +0800

    Add bufferFamilies

--- service/mock_data.go
+++ service/mock_data.go
@@ -39,9 +39,38 @@ func parent(name string, gender uint8, phone string) model.Parent {
 	}
 }
 
-func mockFamilies(seed int64, capacity int, bufferSize int) <-chan []model.Family {
-	var generator = mock_data.NewWithSeed(12)
-	var ch = make(chan []model.Family, capacity)
+func student(name string, gender uint8) model.Student {
 
-	return ch
+}
+
+func mockFamilies(seed int64, count int, bufferSize uint) <-chan []model.Family {
+	var generator = mock_data.NewWithSeed(seed)
+	var names = generator.GetFamilyNames(count)
+	var familyCh = make(chan model.Family)
+	for famNames := range names {
+		var father = famNames[0]
+		var mother = famNames[1]
+		var child1 = famNames[2]
+		var child2 = famNames[3]
+		familyCh <- 
+	}
+
+	return bufferFamilies(familyCh, bufferSize)
+}
+
+func bufferFamilies(ch chan model.Family, bufferSize uint) <-chan []model.Family {
+	var outCh = make(chan []model.Family)
+	buffer := make([]model.Family, bufferSize)
+	for fam := range ch {
+		buffer = append(buffer, fam)
+		if len(buffer) >= int(bufferSize) {
+			outCh <- buffer
+			buffer = make([]model.Family, bufferSize)
+		}
+	}
+	if len(buffer) > 0 {
+		outCh <- buffer
+	}
+	close(outCh)
+	return outCh
 }
